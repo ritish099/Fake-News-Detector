679a335f15056a9f5098ddf2134468d4a37c8175
Tithi Ghosh <65277727+Cosine42@users.noreply.github.com>
2020-09-06 18:35:26 +0530

    Update fake_news_dataset.py

--- datasets/fake_news_dataset.py
+++ datasets/fake_news_dataset.py
@@ -9,12 +9,12 @@ for lab, row in data.iterrows():
     if not str(data.loc[lab, 'Text'])=='nan':
         continue
     
-    file = data.loc[lab, 'filename'][84:]
+    file = data.loc[lab, 'id']+'.txt' 
      
     with open(os.path.join(path, file)) as f:
         content = f.read()
         
     data.loc[lab, 'Text'] = content
 
+data.drop(['filename', 'id'], axis=1, inplace = True) 
 data.to_csv('fake_news_dataset.csv')
-     
